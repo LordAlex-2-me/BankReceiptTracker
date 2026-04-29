e01b24ebb10bcb79114639134a990fbb7f7095cf
LordAlex-2-me <alexanderdeinkoru@gmail.com>
2026-04-29 06:16:10 +0100

    Edited config.py to remove sensitive data

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 # Reads from environment variables on GitHub Actions
 # Falls back to hardcoded values when running locally
-TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN', '8798615404:AAEIneri_D6DExXo0Pp1SFW8V_NyQSiDz5c')
-TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID', '6673920353')
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN', 'your-token-here')
+TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID', 'your-chat-id-here')
